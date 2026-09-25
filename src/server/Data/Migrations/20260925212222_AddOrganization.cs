cd0ac4f5b975f29effb99906d71d6582a14b659a
Sirshak Bohara <boharasirshak101@gmail.com>
2026-09-25 17:07:49 -0500

    db: add organization migration

---
+++ src/server/Data/Migrations/20260925212222_AddOrganization.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyThorneAI.Ats.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrganization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
